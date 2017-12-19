244336dc44df88e41f30d3e74d71a47dea1e253f
Shardul.pathak <Shardul.pathak>
2017-12-19 23:56:38 +0530

    Admob initial commit.

---
+++ AdMobSample/app/src/main/java/com/example/shardulpathak/admobsample/InterstitialAdActivity.java
@@ -0,0 +1,13 @@
+package com.example.shardulpathak.admobsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InterstitialAdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interstitial_ad);
+    }
+}
---
+++ AdMobSample/app/src/main/java/com/example/shardulpathak/admobsample/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.shardulpathak.admobsample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String APP_ID = "ca-app-pub-9390048287444061/3221111032 ";
+
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAdView = (AdView) findViewById(R.id.adView);
+
+        AdRequest adRequest = new AdRequest.Builder()
+                .build();
+        mAdView.loadAd(adRequest);
+
+    }
+
+    @Override
+    protected void onPause() {
+        if (mAdView != null) {
+            mAdView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mAdView != null) {
+            mAdView.resume();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        if (mAdView != null) {
+            mAdView.destroy();
+        }
+        super.onDestroy();
+    }
+}
